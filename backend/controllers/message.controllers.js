d19cb5d9bd8284cc031bc3a401c5c56800a85396
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-05-25 09:56:57 +0530

    Concurrent promises

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -27,8 +27,10 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    await conversation.save();
-    await newMessage.save();
+    // await conversation.save();
+    // await newMessage.save();
+
+    await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
   } catch (error) {
